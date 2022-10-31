4fab644186b08fe7edd3f7b4d567f4d85dcd6c72
Pedro Gallego López <pedrogallegolop@gmail.com>
2022-10-31 11:41:13 +0100

    debug mode removed

--- app.py
+++ app.py
@@ -32,4 +32,4 @@ app.layout = dbc.Container([header,dash.page_container],
                             fluid=False)
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server()
